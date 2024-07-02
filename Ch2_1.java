efc10285575c32b38ecdce3a1637c0cc38aacb04
A12991996 <eugene19990603@gmail.com>
2024-07-02 11:51:31 +0800

    Add files via upload
    我的第一支Java程式

---
+++ Ch2_1.java
@@ -0,0 +1,9 @@
+// Ch2_1, 簡單的Java程式
+public class Ch2_1 {    // 定義public類別Ch2_1
+    public static void main(String[] args) {    // main()函數，主程式開始
+        int num1=2;     // 宣告整數num
+        int num2=3;
+        System.out.println(num1+"cats are running"); // 印出字串及變數內容
+        System.out.println(num2+"dogs are running");
+    }
+} 
