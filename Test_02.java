54b831cff242d7b0cfde6dcf4b91ea5e15bb0368
A12991996 <eugene19990603@gmail.com>
2024-07-03 16:43:03 +0800

    123

---
+++ Test_02.java
@@ -0,0 +1,8 @@
+public class Test_02 {
+    public static void main(String[] args){
+        double celcius = 25.0;
+        double fahrenheit = (celcius * 9/5) + 32;
+
+        System.out.println(celcius + "度攝氏 is equal to " + fahrenheit + "度華氏");
+    }
+}
